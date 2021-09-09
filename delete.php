3107aff190d4bbe5f57ca27b83de11ecba664599
Gowdham M <gowdhammurugesh24@gmail.com>
2021-09-09 22:39:25 +0530

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,26 @@
+<?php
+		include "db.php";
+
+		$conn = mysqli_connect($servername, $user, $password, $database);
+		
+		// Check connection
+		if($conn === false){
+			die("ERROR: Could not connect. "
+				. mysqli_connect_error());
+		}
+
+		$id = $_GET['id'];
+
+		$del = mysqli_query($conn,"DELETE FROM counselling where Choice_Order='$id';");
+
+		if($del)
+        {
+            mysqli_close($conn); // Close connection
+            header("location:crud.php"); // redirects to all records page
+            exit;	
+        }
+        else
+        {
+            echo "Error deleting record"; // display error message if not delete
+        }
+        ?>
