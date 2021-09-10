8c88a277889e4f15ef3e43d29f4928b5fd3ea9c3
Gowdham M <gowdhammurugesh24@gmail.com>
2021-09-10 23:18:25 +0530

    Add files via upload

---
+++ order.php
@@ -0,0 +1,43 @@
+<?php
+		include "db.php";
+
+		$conn = mysqli_connect($servername, $user, $password, $database);
+		
+		// Check connection
+		if($conn === false){
+			die("ERROR: Could not connect. "
+				. mysqli_connect_error());
+		}
+
+		$id = $_GET['id'];
+        $order = $_GET['order'];
+
+        if($order=='up')
+        {
+            mysqli_query($conn,"SET @b=$id;");
+            mysqli_query($conn,"SET @c=$id-1;");
+            mysqli_query($conn,"UPDATE counselling SET id=IF(id=@b, @c, @b) where id in(@b,@c);");
+            mysqli_query($conn,"SET @a:=0;");
+            mysqli_query($conn,"UPDATE counselling SET id=@a:=@a+1 order by id;");
+        }
+
+        if($order=='down')
+        {
+            mysqli_query($conn,"SET @b=$id;");
+            mysqli_query($conn,"SET @c=$id+1;");
+            mysqli_query($conn,"UPDATE counselling SET id=IF(id=@b, @c, @b) where id in(@b,@c);");
+            mysqli_query($conn,"SET @a:=0;");
+            mysqli_query($conn,"UPDATE counselling SET id=@a:=@a+1 order by id;");
+        }
+
+		if($order)
+        {
+            mysqli_close($conn); // Close connection
+            header("location:crud.php"); // redirects to all records page
+            exit;	
+        }
+        else
+        {
+            echo "Error updating record"; // display error message if not delete
+        }
+        ?>
